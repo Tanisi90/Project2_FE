49848dc6337f99b9f94d2b58c89b5c077ede1976
MixerMonochrome <claudiayllanes@gmail.com>
2020-09-24 13:54:43 -0400

    Revert "gitignore and also Hidden Issue"
    This reverts commit b8aa590b03b86f846fe998cbee951ff22f112526.

--- src/app/Components/characters-new/characters-new.component.ts
+++ src/app/Components/characters-new/characters-new.component.ts
@@ -16,7 +16,7 @@ export class CharactersNewComponent implements OnInit {
   intMod:number = -3;
   dexMod:number = -3;
 
-  hidden:boolean;
+  type = "Hidden";
 
   constructor(private rs:RaceService) { }
 
@@ -62,8 +62,6 @@ export class CharactersNewComponent implements OnInit {
       }
       this.rs.getBase(index).subscribe(
         (response:any) => {
-          this.getSpeed(response);
-          this.getProfs(response);
           this.rs.parseLangs(lang,response);
           
           if(subrace == true){
@@ -80,51 +78,6 @@ export class CharactersNewComponent implements OnInit {
       );
   }
 
-  getSpeed(response:any){
-    var speed:HTMLElement = document.getElementById("charSpeed");
-    speed.innerText = response["speed"];
-  }
-
-  getProfs(response:any){
-    var prfs:HTMLElement = document.getElementById("charProfsRace");
-    prfs.innerText="";
-    var profAr:any[] = response["starting_proficiencies"];
-    for(var p of profAr){
-      prfs.innerHTML += "<div class = 'setProfs'>" + p["index"] + "</div>";
-    }
-    var opt:any[] = response["starting_proficiency_options"];
-    if(opt != null){
-      if(opt["choose"] != null){
-        for(var i = 0; i < opt["choose"]; i++){
-          var child = document.createElement("select");
-          child.setAttribute("id","getProf"+(i+1));
-          prfs.appendChild(child);
-          for(var pchoic of opt["from"]){
-            var option = document.createElement("option");
-            option.value = pchoic["index"];
-            option.innerText = pchoic["index"];
-            child.appendChild(option);
-          }
-        }
-      }
-      else{
-        for(var inOpt of opt){
-          for(var i = 0; i < inOpt["choose"]; i++){
-            var child = document.createElement("select");
-            child.setAttribute("id","getProf"+(i+1));
-            prfs.appendChild(child);
-            for(var pchoic of opt["from"]){
-              var option = document.createElement("option");
-              option.value = pchoic["index"];
-              option.innerText = pchoic["index"];
-              child.appendChild(option);
-            }
-          }
-        }
-      }
-    }
-  }
-
   changeLangs(lang:any[]){
     var langs:HTMLElement = document.getElementById("charLangsChange");
     langs.innerHTML = "";
@@ -133,7 +86,7 @@ export class CharactersNewComponent implements OnInit {
     for(let l of lang){
       if(choice == false){
         if(typeof(l) == "string"){
-          langs.innerHTML += "<div class = 'setLangs'>" +  l + "</div>";
+          langs.innerHTML += l + "<br>";
         }
         else if(typeof(l) == "number"){
           for(var i = 0; i < l; i++){
